67253fb91130c0defdbead874d728ca71548d3ea
Slinkyyy <slinky.sthlm@gmail.com>
2021-04-26 17:38:11 +0200

    Conclure update.

--- src/main/java/eu/tribusmc/tribusgiveaways/lang/MessageManager.java
+++ src/main/java/eu/tribusmc/tribusgiveaways/lang/MessageManager.java
@@ -1,32 +1,63 @@
 package eu.tribusmc.tribusgiveaways.lang;
 
+import com.sun.istack.internal.NotNull;
 import eu.tribusmc.tribusgiveaways.Core;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MessageManager {
 
-    private HashMap<String, String> messages = new HashMap<>();
+    /**
+     * @Author Scarmo
+     */
+
+    private final HashMap<String, String> messages;
+
 
     public MessageManager(Core core) {
+        messages = new HashMap<>();
 
         for (String key : core.getConfig().getConfigurationSection("lang").getKeys(false)) {
             messages.put(key, core.getConfig().getString("lang." + key));
         }
-
     }
 
-   public String getMessage(String key) {
-        return messages.get(key);
-   }
 
-    public void sendMessage(Player p, String key) {
+    /**
+     * Gets a message from config
+     *
+     * @param key
+     * @return message from key
+     */
+
+
+    public String getMessage(@NotNull String key) {
+        return messages.get(key);
+    }
+
+    /**
+     * Send a message to a Player
+     *
+     * @param p
+     * @param key
+     */
+
+    public void sendMessage(@NotNull Player p, @NotNull String key) {
         p.sendMessage(getMessage(key));
     }
 
-    public void sendMessage(CommandSender sender, String key) {
+
+    /**
+     * Send a message to CommandSender
+     *
+     * @param sender
+     * @param key
+     */
+
+    public void sendMessage(@NotNull CommandSender sender, @NotNull String key) {
         sender.sendMessage(getMessage(key));
     }
 
