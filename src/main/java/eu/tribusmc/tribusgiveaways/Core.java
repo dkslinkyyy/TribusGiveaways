215ebf027d9b0f0b829fa84918cdde8abc1c777c
Slinkyyy <slinky.sthlm@gmail.com>
2021-04-26 17:19:13 +0200

    Initial commit

---
+++ src/main/java/eu/tribusmc/tribusgiveaways/Core.java
@@ -0,0 +1,16 @@
+package eu.tribusmc.tribusgiveaways;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
